835b1608778d2a23a2997bfda9d71749209f56d3
na-Itms <na.itms76@gmail.com>
2024-03-17 23:40:30 +0100

    Fix incorrect assumption that the ticket time is for the last change.

--- accessor/trac/ticketChange.go
+++ accessor/trac/ticketChange.go
@@ -24,21 +24,6 @@ func sqlForFieldList(fields []TicketChangeType) string {
 	return "(" + fieldListSQL + ")"
 }
 
-// sqlForFirstChange returns the SQL for retrieving details of the first change to a ticket
-func sqlForFirstChange() string {
-	// note: this query requires additional protection against NULL for case where no change exists for ticket
-	changeSQL := `
-		SELECT 0 source,
-			COALESCE(chg0.field, '') field,
-			COALESCE(chg0.oldvalue, '') value,
-			COALESCE(chg0.author, '') author,
-			COALESCE(MIN(CAST(chg0.time*1e-6 AS int8)),0) time
-		FROM ticket_change chg0
-		WHERE chg0.ticket = $1
-		`
-	return changeSQL
-}
-
 // sqlForFirstChangeToEachField returns the SQL for retrieving details of the first change to each of a set of fields of a ticket
 func sqlForFirstChangeToEachField(fields []TicketChangeType) string {
 	changeSQL := `
@@ -56,7 +41,7 @@ func sqlForFirstChangeToEachField(fields []TicketChangeType) string {
 	return changeSQL
 }
 
-// sqlForTicketTableField returns the SQL for retrieving each of set of fields of a ticket in the same format as the the other queries above
+// sqlForTicketTableField returns the SQL for retrieving each of set of fields of a ticket in the same format as the other queries above
 func sqlForTicketTableField(fieldIndex int, field TicketChangeType) string {
 	table := fmt.Sprintf("t%d", fieldIndex)
 	strField := string(field)
@@ -86,19 +71,15 @@ func (accessor *DefaultAccessor) getInitialTicketChanges(ticketID int64, handler
 	// 2. (if no recorded change for field) in the appropriate field of the main ticket table
 	//
 	// We retrieve this using a complex SQL query which returns the following columns:
-	// - source: "source" of data - 0 (see below), 1 or 2 (see above)
+	// - source: "source" of data - 1 or 2 (see above)
 	// - field: name of field
 	// - value: initial value for field
 	// - author: author for field
 	// - time: timestamp for value (converted to Gitea precision)
-	//
-	// In addition to sources 1 and 2 above we also return a source 0 which contains the earliest of all changes to the ticket.
-	// If this exists, its author and time values are used to sign any data returned from the main ticket table (source 2 above).
-	// This is because the author and time in the main ticket table refer to the latest change to the ticket table not the earliest.
 	initialValueSQL := `
 		SELECT source, field, value, author, time FROM 
 		(`
-	initialValueSQL = initialValueSQL + sqlForFirstChange() + `UNION` + sqlForFirstChangeToEachField(initialTicketChangeFields)
+	initialValueSQL = initialValueSQL + sqlForFirstChangeToEachField(initialTicketChangeFields)
 	for fieldIndex, field := range initialTicketChangeFields {
 		initialValueSQL = initialValueSQL + `UNION` + sqlForTicketTableField(fieldIndex, field)
 	}
@@ -110,8 +91,6 @@ func (accessor *DefaultAccessor) getInitialTicketChanges(ticketID int64, handler
 		return err
 	}
 
-	var firstChangeAuthor = ""
-	var firstChangeTime = int64(0)
 	var source1Fields = make(map[string]bool)
 	for rows.Next() {
 		var source int
@@ -123,12 +102,6 @@ func (accessor *DefaultAccessor) getInitialTicketChanges(ticketID int64, handler
 		}
 
 		switch source {
-		case 0:
-			// source 0 change: this just gives us the author and time of the first change
-			// - where there a ticket has no changes, author will be "" and time 0 - the effect of this will be harmless
-			firstChangeAuthor = author
-			firstChangeTime = time
-			continue
 		case 1:
 			// source 1 change: earliest recorded change for a given field
 			// this type of change takes precedence over changes from the main ticket table (source 2)
@@ -139,14 +112,6 @@ func (accessor *DefaultAccessor) getInitialTicketChanges(ticketID int64, handler
 			// - this is only important if we haven't found an explicit recorded change for our field (source 1)
 			if source1Fields[field] {
 				continue
-			}
-
-			// replace author and time with those from first change (if any)
-			if firstChangeAuthor != "" {
-				author = firstChangeAuthor
-			}
-			if firstChangeTime > 0 && firstChangeTime < time {
-				time = firstChangeTime
 			}
 		}
 
